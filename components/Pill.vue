0b77f84eb01eebaf9a7b981e0c23a83cbfa873d3
grountish <grountish@gmail.com>
2022-10-27 19:02:30 +0200

    fix about title

--- components/Pill.vue
+++ components/Pill.vue
@@ -1,7 +1,9 @@
 <template>
+    <div class="flex">
         <h5 :class="['text-center px-5 text-3_5xl pt-5 pb-2 m-auto rounded-full leading-none uppercase my-12 shadow', color ? 'bg-darkyellow text-sharppurple' : 'bg-white text-black']">
             {{ text }}
         </h5>
+    </div>
 </template>
 
 <script>
