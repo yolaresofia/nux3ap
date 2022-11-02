af7430a5fde768cf143ce078bda96331172ecf7c
Anna Ehrnsperger <hallo@annaehrnsperger.de>
2022-11-02 18:52:08 +0100

    refactor comments markup

--- components/Comments.vue
+++ components/Comments.vue
@@ -1,53 +1,42 @@
 <template>
-    <div v-if="showComments">
-        <div class="flex">
-            <div class="text-2xl fixed bottom-4 right-8 md:right-16 cursor-pointer z-50">
-                <!-- <div>
-                    <span v-if="hasNewComments">🆕</span>
-                </div>
-                <div>
-                    <span v-if="hasNewComments" @click="stackComments">⬇️</span>
-                </div> -->
-
-                <Transition :css="false" @enter="onDots" @leave="onDotsLeave">
-                    <button v-if="closeStack" @click="closeStack = false" class="translate-y-[200%] flex space-x-2 bg-lightblack p-3 px-4 rounded-full">
-                        <span class="inline-block opacity-0 w-[12px] h-[12px] rounded-full bg-white"></span>
-                        <span class="inline-block opacity-0 w-[12px] h-[12px] rounded-full bg-white"></span>
-                        <span class="inline-block opacity-0 w-[12px] h-[12px] rounded-full bg-white"></span>
-                    </button>
-                </Transition>
-            </div>
-
-            <!-- setting dynamic min-h since if you scroll to bottom
-                 you can't click on the buttons or themeswitcher
-                maybe there's a better way?
-            -->
-            <TransitionGroup
-                name="commentsContainer"
-                tag="div"
-                :class="[
-                    'flex flex-col cursor-pointer w-full md:w-[30rem] fixed bottom-4 md:right-0 items-center md:justify-end md:p-4 md:pr-12 md:pb-0',
-                    stacked ? 'min-h-[20px] z-50' : 'min-h-[250px] z-40',
-                ]"
-                @enter="onEnter"
-                :css="false"
-                @click="stacked = true"
-                data-comments-container
+    <section>
+        <TransitionGroup
+            v-if="commentArr.length > 0"
+            tag="div"
+            name="commentsContainer"
+            class="fixed bottom-4 px-4 md:px-0 md:right-8 flex flex-col-reverse w-full md:w-[30rem]"
+            :class="{ 'cursor-pointer z-40': !open, 'z-50': open }"
+            :css="false"
+            @enter="onEnter"
+            @click="open = true"
+            data-comments-container
+        >
+            <div
+                class="text-white sans-serif text-sm rounded-lg bg-lightblack p-2 w-full md:w-[30rem] origin-center mt-3 bottom-[150px] right-4 md:bottom-4 md:right-8"
+                :class="{ 'comment-width | fixed': !openFinished, static: openFinished, hidden: closeStack }"
+                v-for="(comment, i) in commentArr"
+                :key="comment"
+                data-comment
+                :data-index="(i - commentArr.length) * -1"
             >
-                <div
-                    v-for="(comment, i) in commentArr"
-                    :key="comment"
-                    class="comment-width text-sm p-10 rounded-xl bg-lightblack font-mono grid text-white md:w-[30rem] origin-bottom left-4 bottom-[150px] md:bottom-4 md:left-auto md:right-8 mt-3"
-                    :class="{ 'comment-width | fixed pointer-events-none': !stackedFinished, 'static md:mr-8': stackedFinished, hidden: closeStack }"
-                    data-comment
-                    :data-index="(i - commentArr.length) * -1"
-                    @click="closeStack = true"
-                >
-                    <span class="font-mono">From IG </span> <span class="font-mono">{{ comment?.user }} just commented on WOMB post </span> <span class="font-mono">{{ comment?.title }}</span>
-                </div>
-            </TransitionGroup>
-        </div>
-    </div>
+                <p>From IG</p>
+                <p>{{ comment?.user }} just commented on WOMB's post:</p>
+                <p class="pb-10">{{ comment?.title }}</p>
+                <p class="text-darkgray" :class="{ 'opacity-0': commentArr.length === 1 || open }">
+                    {{ commentArr.length - 1 }} {{ commentArr.length === 2 ? 'more notification' : 'more notifications' }}
+                </p>
+            </div>
+            <button v-if="open && !closeStack" class="text-right font-mono text-sm text-black uppercase" @click="closeStack = true">Close</button>
+        </TransitionGroup>
+
+        <Transition :css="false" @enter="onDots" @leave="onDotsLeave">
+            <button v-if="closeStack" @click="closeStack = false" class="fixed bottom-4 right-4 md:right-8 translate-y-[200%] flex space-x-2 bg-lightblack p-3 px-4 rounded-full z-50">
+                <span class="inline-block opacity-0 w-[12px] h-[12px] rounded-full bg-white"></span>
+                <span class="inline-block opacity-0 w-[12px] h-[12px] rounded-full bg-white"></span>
+                <span class="inline-block opacity-0 w-[12px] h-[12px] rounded-full bg-white"></span>
+            </button>
+        </Transition>
+    </section>
 </template>
 
 <script setup>
@@ -58,8 +47,8 @@ import { gsap } from 'gsap'
 const store = useStore()
 const currentComment = useState('currentComment', () => store.comments[0])
 const index = useState('index', () => 0)
-const stacked = useState('stacked', () => false)
-const stackedFinished = useState('stackedFinished', () => false)
+const open = useState('open', () => false)
+const openFinished = useState('openFinished', () => false)
 const closeStack = useState('closeStack', () => false)
 const commentArr = useState('commentArr', () => [])
 const showComments = useState('showComments', () => false)
@@ -70,7 +59,7 @@ const onEnter = (el, done) => {
         el,
         {
             opacity: 0,
-            y: 40,
+            y: open.value ? -40 : 40,
         },
         {
             opacity: 1,
@@ -118,13 +107,13 @@ if (store.comments.length > 1) {
         if (!hasNewComments.value && showComments.value) {
             hasNewComments.value = true
         }
-    }, rand * 1000)
+    }, rand * 500)
 }
 onUnmounted(() => {
     clearInterval(intervalC)
 })
 watch(commentArr.value, () => {
-    if (stacked.value) return
+    if (open.value) return
 
     const comments = selectAll('[data-comment]')
 
@@ -133,16 +122,16 @@ watch(commentArr.value, () => {
     })
 })
 
-watch(stacked, () => {
+watch(open, () => {
     let mm = gsap.matchMedia()
 
-    if (!stacked.value) return
+    if (!open.value) return
 
     const commentContainer = select('[data-comments-container]')
     const comments = selectAll('[data-comment]')
     const MARGIN = 12
 
-    mm.add('(max-width: 640px)', () => {
+    mm.add('(max-width: 768px)', () => {
         gsap.to(commentContainer, { y: 150, duration: 0.2 })
     })
 
@@ -157,7 +146,7 @@ watch(stacked, () => {
                 gsap.set(commentContainer, { y: 0 })
                 gsap.set(comment, { bottom: '1rem' })
                 comment.removeAttribute('style')
-                stackedFinished.value = true
+                openFinished.value = true
             },
         })
     })
@@ -174,7 +163,7 @@ watch(store, () => {
 
         const commentContainer = select('[data-comments-container]')
 
-        mm.add('(max-width: 640px)', () => {
+        mm.add('(max-width: 768px)', () => {
             gsap.to(commentContainer, { y: 150, duration: 0.2 })
         })
     }
@@ -182,7 +171,7 @@ watch(store, () => {
 </script>
 
 <style scoped>
-@media (max-width: 640px) {
+@media (max-width: 768px) {
     .comment-width {
         width: calc(100% - 2rem);
     }
